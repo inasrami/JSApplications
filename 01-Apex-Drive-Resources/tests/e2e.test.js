539a514099361255e67515ac21ad3fac9a617f57
Inas Rami <inkorami27@gmail.com>
2025-12-06 16:54:45 +0200

    test3

--- 01-Apex-Drive-Resources/tests/e2e.test.js
+++ 01-Apex-Drive-Resources/tests/e2e.test.js
@@ -1,4 +1,4 @@
-const { chromium } = require('playwright-chromium');
+const { chromium } = require('playwright'); 
 const { expect } = require('chai');
 
 // const userApplicationHttpPort = "#userApplicationHttpPort#";
