49816b9fc1ae803fc480f5e52a59f99c3492acf8
Inas Rami <inkorami27@gmail.com>
2026-02-10 22:30:31 +0200

    1.1

---
+++ 02-JS-Applications-Asynchronous-Programming-Lab-Resources/01.XHR/scripts/app.js
@@ -0,0 +1,9 @@
+function loadRepos() {
+   fetch('https://api.github.com/users/testnakov/repos')
+      .then(res => res.text())
+      .then(data => {
+         const output = document.getElementById('res');
+         output.textContent = data;
+      }
+);
+}
